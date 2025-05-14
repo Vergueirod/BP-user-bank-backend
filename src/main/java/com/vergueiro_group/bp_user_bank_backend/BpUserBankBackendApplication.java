7e6e41ed8d0ebb13f7ce04b26d4350d1ced5fb51
Vergueirod <98171411+Vergueirod@users.noreply.github.com>
2025-05-14 20:33:56 -0300

    Cleanning test code + Create initial packages

--- src/main/java/com/vergueiro_group/bp_user_bank_backend/BpUserBankBackendApplication.java
+++ src/main/java/com/vergueiro_group/bp_user_bank_backend/BpUserBankBackendApplication.java
@@ -1,9 +1,6 @@
 package com.vergueiro_group.bp_user_bank_backend;
-
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class BpUserBankBackendApplication {
@@ -11,17 +8,6 @@ public class BpUserBankBackendApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(BpUserBankBackendApplication.class, args);
-
-		String get_name;
-
-		User u1 = new User("Diego Vergueiro", "42260554890", "diego.ssv@outlook.com", "12982027359", "Oic224");
-
-		get_name = u1.getFullName();
-		System.out.println("get_name: " + get_name);
-
-		u1.setFullName("Felipe");
-		get_name = u1.getFullName();
-		System.out.println("get_name: " + get_name);
 
 	}
 }
---
+++ src/main/java/com/vergueiro_group/bp_user_bank_backend/domain/model/User.java
@@ -0,0 +1,10 @@
+package com.vergueiro_group.bp_user_bank_backend.domain.model;
+
+public class User {
+    private String fullname;
+    private String cpf;
+    private String email;
+    private String phone;
+    private String password;
+
+}
